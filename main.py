6bf2b31370a28f10125d3920c00457621d97bd37
Kelvin Lim <102139658+Kayeel98@users.noreply.github.com>
2022-04-17 06:32:28 +0000

    Update 5.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random 
 print("Hello World")
-
+print()
 
 def age_game():
 	age = input("Your age please: ")
@@ -8,7 +8,7 @@ def age_game():
 
 	randomnumber = random.randint(1,100)
 	randomnumber = int(randomnumber)
-
+	print()
 	print("My age is {0}".format(randomnumber))
 	print("Player age is {0}".format(age))
 	print()
@@ -20,15 +20,15 @@ def age_game():
 		print("We are the samge age, bet we have similar 				hobbies!")
 	print()
 	if age-randomnumber >=20 :
-		print("How is it like being old? HAHA")
 		print("Hello, Mr Boomer!")
+		print("How is it like being old? HAHA")
 	elif randomnumber-age >= 20:
-		print("I am older than you!")
 		print("Hello, Zoomer")
+		print("I am older than you!")	
 	else:
 		print("We are about the same age, similar interest perhaps?")
 
 age_game()
 print()
-print("press run to try again!")
+print("Press run to try again!")
 
