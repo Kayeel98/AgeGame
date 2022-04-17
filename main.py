96c932732c1b880d507fd525f7d1824070a3dce0
Kelvin Lim <102139658+Kayeel98@users.noreply.github.com>
2022-04-17 06:10:52 +0000

    Update 3.0

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 import random 
 print("Hello World")
 
-randomnumber = random.randint(1,100)
 
-print("My age is {0}".format(randomnumber))
+def age_game():
+	age = input("Your age please: ")
+	age = int(age)
+
+	randomnumber = random.randint(1,100)
+	randomnumber = int(randomnumber)
+
+	print("My age is {0}".format(randomnumber))
+	print("Your age is {0}".format(age))
+	print()
+	if age > randomnumber:
+		print("Looks like you're older than me !")
+	elif randomnumber > age:
+		print("Youngling seems like you have more ways to go...")
+	else:
+		print("We are the samge age, bet we have similar 				hobbies!")
+	print()
+	if age-randomnumber <= 20:
+		print("you could be my father! HAHA")
+		print("Hello, Mr Boomer!")
+	elif age-randomnumber >= 40:
+			print("you could be my grandfather! HAHA")
+			print("Hello, Mr Boomer!")
+	elif randomnumber-age <= 20:
+		print("I could be your father! HAHA")
+		print("Hello, Zoomer")
+	elif randomnumber-age >= 40:
+		print("I could be your grandfather! HAHA")
+		print("Hello, Zoomer")
+	else:
+		print("We are about the same age, similar interest perhaps")
+
+
+
+for i in range(3):
+	age_game()
+	print()
